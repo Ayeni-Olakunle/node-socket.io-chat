72e9f3c32df03debdb1664072ae6ee63374a5da4
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-09 15:36:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require("socket.io")
+const cors = require('cors')
+
+const serve = http.createServer(app)
+app.use(cors())
+
+const io = new Server(serve, {
+    cors: {
+        origin: 'http://localhost:3000',
+        method: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`)
+
+    socket.on("send_message", (data) => {
+        console.log(data)
+        socket.broadcast.emit("receive_message", data)
+    })
+})
+
+serve.listen(3001, () => {
+    console.log("listening on port 3001")
+})
